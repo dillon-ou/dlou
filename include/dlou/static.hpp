10d32f58d268358ca6d4303e663931022df24d47
Dillon <dillon.ou@mitake.com.tw>
2023-01-04 15:37:34 +0800

    Add mem_obj

--- include/dlou/static.hpp
+++ include/dlou/static.hpp
@@ -34,4 +34,53 @@ struct remove_member<M T::*> {
 template<class T>
 using remove_member_t = typename remove_member<T>::type;
 
+template<auto MemberObjPtr>
+class mem_obj
+{
+public:
+	using member_object_pointer = decltype(MemberObjPtr);
+	using value_type = remove_member_t<member_object_pointer>;
+	using member_type = member_t<member_object_pointer>;
+	using pointer = value_type*;
+	using const_pointer = const value_type*;
+	using reference = value_type&;
+	using const_reference = const value_type&;
+
+	member_type& operator ()(reference r) const {
+		return r.*MemberObjPtr;
+	}
+
+	const member_type& operator ()(const_reference r) const {
+		return r.*MemberObjPtr;
+	}
+
+	member_type* operator ()(pointer p) const {
+		return &(p->*MemberObjPtr);
+	}
+
+	const member_type* operator ()(const_pointer p) const {
+		return &(p->*MemberObjPtr);
+	}
+
+	ptrdiff_t offset_of() const {
+		return (ptrdiff_t)(&(((pointer)0)->*MemberObjPtr));
+	}
+
+	reference operator [](member_type& m) const {
+		return *(pointer)(((char*)&m) - offset_of());
+	}
+
+	const_reference operator [](const member_type& m) const {
+		return *(pointer)(((char*)&m) - offset_of());
+	}
+
+	pointer operator [](member_type* p) const {
+		return (pointer)(((char*)p) - offset_of());
+	}
+
+	const_pointer operator [](const member_type* p) const {
+		return (pointer)(((char*)p) - offset_of());
+	}
+};
+
 } // namespace dlou
